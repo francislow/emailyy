58360c24715e5df58192fede71e10e5d524be429
Francis <francislow_6@hotmail.com>
2020-01-21 16:15:21 +0800

    test fix deploy #2

--- index.js
+++ index.js
@@ -29,29 +29,29 @@ app.use(passport.session());
 // Set up route handling
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
-// if (process.env.NODE_ENV === 'production') {
-//   // Express will serve files like main.css main.js
-//   app.use(express.static('client/build'));
-
-//   // Express will serve index.html file if it doesnt recognise path
-//   const path = require('path');
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
-
 if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
+  // Express will serve files like main.css main.js
   app.use(express.static('client/build'));
 
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route
+  // Express will serve index.html file if it doesnt recognise path
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+// if (process.env.NODE_ENV === 'production') {
+//   // Express will serve up production assets
+//   // like our main.js file, or main.css file!
+//   app.use(express.static('client/build'));
+
+//   // Express will serve up the index.html file
+//   // if it doesn't recognize the route
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 // Express tells node to listen for requests at PORT
 const PORT = process.env.PORT || 5000; // looks for underlying environment for the port that heroku assigned || goes to port 5000
