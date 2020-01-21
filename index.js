dba4fe6f13734b4be4a3014362c64158c0eab808
Francis <francislow_6@hotmail.com>
2020-01-21 16:25:34 +0800

    final deployment fix

--- index.js
+++ index.js
@@ -40,19 +40,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-// if (process.env.NODE_ENV === 'production') {
-//   // Express will serve up production assets
-//   // like our main.js file, or main.css file!
-//   app.use(express.static('client/build'));
-
-//   // Express will serve up the index.html file
-//   // if it doesn't recognize the route
-//   const path = require('path');
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
-
 // Express tells node to listen for requests at PORT
 const PORT = process.env.PORT || 5000; // looks for underlying environment for the port that heroku assigned || goes to port 5000
 app.listen(PORT, () => {
