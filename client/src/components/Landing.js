033bf1166be1802cc77c717759d4cbbde5342354
Francis <francislow_6@hotmail.com>
2020-02-01 03:09:02 +0800

    Fix logged in user directed to '/' url issue

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
-export default function Landing() {
+function Landing({ history }) {
+  useEffect(() => {
+    axios.get('/api/current_user').then(user => {
+      if (user.data !== "") {
+        history.push('/surveys');
+      }
+    });
+  }, []);
+
   return (
-    <div style={{ textAlign:'center'}}>
+    <div style={{ textAlign: 'center' }}>
       <h1>EMAILY!</h1>
       Collect feedback from your users with ease
     </div>
   );
 }
+
+export default withRouter(Landing);
