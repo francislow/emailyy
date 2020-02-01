42f036f94e2e94f8f37d546a28aede6ef6ae73f1
Francis <francislow_6@hotmail.com>
2020-02-01 14:50:08 +0800

    Improve invalid email message

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -10,7 +10,7 @@ export default emails => {
     .filter(email => re.test(email) === false);
 
   if (invalidEmails.length) {
-    return `These emails are invalid: ${invalidEmails}`;
+    return `These emails are invalid: ${invalidEmails}. Please ensure that there are in this format example@examplecom, example@examplecom`;
   }
 
   return;
